c9f9828f2fdf99b712e60573b8fc340f9442b70a
John D. Johnson <jdjohn215@gmail.com>
2020-05-08 18:16:19 -0500

    fix col arguments

--- R/Crosstab.R
+++ R/Crosstab.R
@@ -50,14 +50,14 @@ crosstab <- function(df, x, y, weight, remove = "", n = TRUE, pct_type = "row", 
                 n = first(total)) %>%
       # Remove values included in "remove" string
       filter(!str_to_upper({{x}}) %in% str_to_upper(remove),
-             !str_to_upper({{y}}) %in% str_to_upper(remove))
+             !str_to_upper({{y}}) %in% str_to_upper(remove)) %>%
+      ungroup()
 
     # spread if format = wide
     if(format == "wide"){
       d.output <- d.output %>%
         # Spread so x is rows and y is columns
-        complete({{x}}, fill = list(pct = 0)) %>%
-        pivot_wider(names_from = {{x}}, values_from = pct) %>%
+        spread(key = {{y}}, value = pct, fill = 0) %>%
         # move total row to end
         select(-one_of("n"), one_of("n")) %>%
         ungroup()
@@ -92,8 +92,7 @@ crosstab <- function(df, x, y, weight, remove = "", n = TRUE, pct_type = "row", 
       total.row <- d.output %>%
         group_by({{y}}, n) %>%
         summarise() %>%
-        complete({{y}}, fill = list(n = 0)) %>%
-        pivot_wider(names_from = {{y}}, values_from = n) %>%
+        pivot_wider(names_from = {{y}}, values_from = n, values_fill = list(pct = 0)) %>%
         mutate({{x}} := "n")
 
       # spread the output table
@@ -101,8 +100,7 @@ crosstab <- function(df, x, y, weight, remove = "", n = TRUE, pct_type = "row", 
         # drop the n column
         select(-n) %>%
         # spread so x is rows and y is columns
-        complete({{y}}, fill = list(pct = 0)) %>%
-        pivot_wider(names_from = {{y}}, values_from = n) %>%
+        spread(key = {{y}}, value = pct, fill = 0)
 
       # if n = TRUE, then add then n row
       # this causes the response column to switch from factor to character
@@ -137,8 +135,7 @@ crosstab <- function(df, x, y, weight, remove = "", n = TRUE, pct_type = "row", 
     if(format == "wide"){
       d.output <- d.output %>%
         # Spread so x is rows and y is columns
-        complete({{y}}, fill = list(pct = 0)) %>%
-        pivot_wider(names_from = {{y}}, values_from = pct) %>%
+        spread(key = {{y}}, value = pct, fill = 0) %>%
         # move total row to end
         select(-one_of("n"), one_of("n")) %>%
         ungroup()
